253e22dcdceeffa55d6f1e02ff7cc2f2362ca725
Aki Mäkinen <aki.makinen@outlook.com>
2016-04-02 23:40:04 +0300

    Added some express middleware for 'real-life' middleware testing. Also added first tests for pre and post middleware.

--- test/prepostMiddleware.js
+++ test/prepostMiddleware.js
@@ -1,3 +1,54 @@
 /**
  * Created by xc- on 2.4.2016.
  */
+var expressopot = require('../index');
+var chai = require('chai');
+
+var expect = chai.expect;
+var assert = chai.assert;
+
+var _ = require('lodash');
+
+describe('Middleware', function () {
+  var express = require('express');
+  var bodyParser = require('body-parser');
+  var cookieParser = require('cookie-parser');
+  var logger = require('morgan');
+  var path = require('path');
+  var middleware = [
+    logger('dev'),
+    bodyParser.json(),
+    cookieParser()
+  ];
+  var names = function(collection) {
+    return _.map(collection, function(val) {
+      return val.name;
+    });
+  };
+  
+  describe('Pre', function() {
+    it('Should be found from express when defined', function() {
+      var config = {
+        middleware: {
+          pre: middleware
+        }
+      };
+      var app = expressopot(express(), config);
+      var app_mw_names = names(app._router.stack);
+      expect(app_mw_names).to.include.members(names(middleware));
+    });
+  });
+  
+  describe('Post', function() {
+    it('Should be found from express when defined', function() {
+      var config = {
+        middleware: {
+          post: middleware
+        }
+      };
+      var app = expressopot(express(), config);
+      var app_mw_names = names(app._router.stack);
+      expect(app_mw_names).to.include.members(names(middleware));
+    });
+  });
+});
