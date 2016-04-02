f0a840b5377c7aecd4d37b46b6e666d4190a24e6
Aki Mäkinen <aki.makinen@outlook.com>
2016-04-03 00:19:57 +0300

    Placeholders for some tests

--- test/parameterMiddleware.js
+++ test/parameterMiddleware.js
@@ -1,3 +1,13 @@
 /**
  * Created by xc- on 2.4.2016.
  */
+var chai = require('chai');
+
+var expect = chai.expect;
+var assert = chai.assert;
+
+describe('Parameters', function () {
+  it('Should be a placeholder', function() {
+  
+  });
+});
--- test/routeHandling.js
+++ test/routeHandling.js
@@ -1,3 +1,13 @@
 /**
  * Created by xc- on 2.4.2016.
  */
+var chai = require('chai');
+
+var expect = chai.expect;
+var assert = chai.assert;
+
+describe('Routes', function () {
+  it('Should be a placeholder', function() {
+
+  });
+});
