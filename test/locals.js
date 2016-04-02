ab6cf4b5caa657f5d5af844a57020150c2b51b4f
Aki Mäkinen <aki.makinen@outlook.com>
2016-04-02 21:25:48 +0300

    Added mocha, chai + some express packages for testing purposes. Wrote tests for config.locals.

--- test/locals.js
+++ test/locals.js
@@ -1,3 +1,36 @@
 /**
  * Created by xc- on 2.4.2016.
  */
+var expressopot = require('../index');
+var chai = require('chai');
+
+var expect = chai.expect;
+var assert = chai.expect;
+
+describe('Locals', function() {
+  var express = require('express');
+
+  it('should contain locals when they are defined in the configuration file', function() {
+    var config = {
+      locals: {
+        testString: "String",
+        testNumber: 1138,
+        testFunction: function() { return "Foobar"; }
+      }
+    };
+
+    var app = expressopot(express(), config);
+    expect(app.locals).to.deep.equal(config.locals);
+    expect(app.locals.testString).to.equal(config.locals.testString);
+    expect(app.locals.testNumber).to.equal(config.locals.testNumber);
+    expect(app.locals.testFunction()).to.equal(config.locals.testFunction());
+  });
+
+  it('should throw an exception if the locals is not an object', function() {
+    var config = {
+      locals: function() { return 42; }
+    };
+
+    expect(expressopot.bind(expressopot, express(), config)).to.throw(TypeError);
+  });
+});
